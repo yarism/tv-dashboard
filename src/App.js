74e257ffa8c38c97b6952d451eac4f6010832f34
Joakim Johansson <johansson.joakim@gmail.com>
2019-06-05 16:38:11 +0200

    Fixed court numbers

--- src/App.js
+++ src/App.js
@@ -13,18 +13,18 @@ function App() {
           <h1 className="headline">Banor</h1>
           <h2>Padel</h2>
           <ul className="grid">
-            <Spot />
-            <Spot />
-            <Spot />
-            <Spot available={true} />
-            <Spot />
-            <Spot available={true} />
-            <Spot available={true} />
-            <Spot />
-            <Spot />
-            <Spot available={true} />
-            <Spot />
-            <Spot available={true} />
+            <Spot court={1} />
+            <Spot court={2} />
+            <Spot court={3} />
+            <Spot court={4} available={true} />
+            <Spot court={5} />
+            <Spot court={6} available={true} />
+            <Spot court={7} available={true} />
+            <Spot court={8} />
+            <Spot court={9} />
+            <Spot court={10} available={true} />
+            <Spot court={11} />
+            <Spot court={12} available={true} />
           </ul>
         </div>
       </main>
--- src/components/Spot.js
+++ src/components/Spot.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const spot = ({available}) => {
+const spot = ({court, available}) => {
     return (
             <li className={"grid__card " + (available ? "grid__card--available" : "")}>
-                <h3 className="card__headline">Bana 12 - 13:00 - 14:00</h3>
+                <h3 className="card__headline">Bana {court} - 13:00 - 14:00</h3>
                 <div className="player__container">
                     {available &&
                         <React.Fragment>
