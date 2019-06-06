3712b4165cc23bc9527740679e34422ee723be4a
Joakim Johansson <johansson.joakim@gmail.com>
2019-06-06 10:47:30 +0200

    minor

--- src/App.js
+++ src/App.js
@@ -9,24 +9,22 @@ function App() {
     <div className="app">
       <Clock />
       <main className="main">
-        <div className="blaha">
-          <h1 className="headline">Banor</h1>
-          <h2>Padel</h2>
-          <ul className="grid">
-            <Spot court={1} />
-            <Spot court={2} />
-            <Spot court={3} />
-            <Spot court={4} available={true} />
-            <Spot court={5} />
-            <Spot court={6} available={true} />
-            <Spot court={7} available={true} />
-            <Spot court={8} />
-            <Spot court={9} />
-            <Spot court={10} available={true} />
-            <Spot court={11} />
-            <Spot court={12} available={true} />
-          </ul>
-        </div>
+        <h1 className="headline">Banor</h1>
+        <h2>Padel</h2>
+        <ul className="grid">
+          <Spot court={1} />
+          <Spot court={2} />
+          <Spot court={3} />
+          <Spot court={4} available={true} />
+          <Spot court={5} />
+          <Spot court={6} available={true} />
+          <Spot court={7} available={true} />
+          <Spot court={8} />
+          <Spot court={9} />
+          <Spot court={10} available={true} />
+          <Spot court={11} />
+          <Spot court={12} available={true} />
+        </ul>
       </main>
       <Sidemenu />
     </div>
